1f62741100eab756a643f7aec6d8141c5b7452be
Ankit Srivastava <sankit@student.ethz.ch>
2017-05-10 16:45:06 +0200

    bin_convolution: loop unrolled, scalar replacement, filter convolves two images simultaneously

--- src/conv_layer.c
+++ src/conv_layer.c
@@ -25,21 +25,157 @@ void convolution(tensor* input_t, tensor* conv_t, int batch_size,
 void bin_convolution(tensor* input_t, tensor* conv_t, int batch_size,
 	int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], double alphas[NUM_FILS], tensor fil_b, int base, int shuffle_index[]){
 
-	for (int b = 0; b < batch_size; ++b)
+	for (int b = 0; b < batch_size; b=b+2)
 	{
+		// load 2 current image
+		int cur_image1 = shuffle_index[b+  base];
+		int cur_image2 = shuffle_index[b+1+base];
+
 		for (int f = 0; f < NUM_FILS; ++f)
 		{
 			for (int i = 0; i < N_ROWS_CONV; ++i)
 			{
 				for (int j = 0; j < N_ROWS_CONV; ++j)
 				{
-					(conv_t->data)[offset(conv_t,b,j,i,f)] = bin_convolve(input_t, i, j, b+base, 
-																fil_bin_w, alphas, fil_b, f, shuffle_index);
+					bin_convolve(input_t, conv_t, b, i, j, cur_image1, cur_image2,
+																fil_bin_w, alphas, fil_b, f);
 				}
 			}
 			
 		}
 	}
+}
+
+void bin_convolve(tensor* t, tensor* conv_t, int b, int r, int c, int cur_image1, int cur_image2, 
+		int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], double alphas[NUM_FILS], tensor fil_b, int f){
+
+	double conv_val1, conv_val2, conv_val3, conv_val4, conv_val5, conv_val6, conv_val7, conv_val8;
+	double conv_img1, conv_img2;
+	conv_val1 = 0;
+	conv_val2 = 0;
+	conv_val3 = 0;
+	conv_val4 = 0;
+	conv_val5 = 0;
+	conv_val6 = 0;
+	conv_val7 = 0;
+	conv_val8 = 0;
+
+	int i, j;
+	for (i = 0; i+1 < FIL_ROWS; i=i+2)
+	{
+		for (j = 0; j+1 < FIL_COLS; j=j+2)
+		{
+			double mat1, mat2, mat3, mat4;
+			mat1 = (t->data)[offset(t, cur_image1, c+j  ,   r+i, 0)];
+			mat2 = (t->data)[offset(t, cur_image1, c+j+1,   r+i, 0)];
+			mat3 = (t->data)[offset(t, cur_image1, c+j,   r+i+1, 0)];
+			mat4 = (t->data)[offset(t, cur_image1, c+j+1, r+i+1, 0)];
+
+			double mat5, mat6, mat7, mat8;
+			mat5 = (t->data)[offset(t, cur_image2, c+j  ,   r+i, 0)];
+			mat6 = (t->data)[offset(t, cur_image2, c+j+1,   r+i, 0)];
+			mat7 = (t->data)[offset(t, cur_image2, c+j,   r+i+1, 0)];
+			mat8 = (t->data)[offset(t, cur_image2, c+j+1, r+i+1, 0)];
+
+			int fil1, fil2, fil3, fil4;
+			fil1 = fil_bin_w[f][i  ][j  ];
+			fil2 = fil_bin_w[f][i  ][j+1];
+			fil3 = fil_bin_w[f][i+1][j  ];
+			fil4 = fil_bin_w[f][i+1][j+1];
+
+			if (fil1 == 1)
+			{
+
+				conv_val1 += mat1;
+				conv_val5 += mat5;
+			}
+			else
+			{
+				conv_val1 -= mat1;
+				conv_val5 -= mat5;
+			}
+
+			if (fil2 == 1)
+			{
+
+				conv_val2 += mat2;
+				conv_val6 += mat6;
+			}
+			else
+			{
+				conv_val2 -= mat2;
+				conv_val6 -= mat6;
+			}
+
+			if (fil3 == 1)
+			{
+
+				conv_val3 += mat3;
+				conv_val7 -+ mat7;
+			}
+			else
+			{
+				conv_val3 -= mat3;
+				conv_val7 -= mat7;
+			}
+
+			if (fil4 == 1)
+			{
+
+				conv_val4 += mat4;
+				conv_val8 += mat8;
+			}
+			else
+			{
+				conv_val4 -= mat4;
+				conv_val8 -= mat8;
+			}
+		}
+	}
+
+	conv_img1 = conv_val1 + conv_val2 + conv_val3 + conv_val4;
+	conv_img2 = conv_val5 + conv_val6 + conv_val7 + conv_val8;
+
+	i=i-2;
+	j=j-2;
+	for (; i < FIL_ROWS; ++i)
+	{
+		for (; j < FIL_COLS; ++j)
+		{
+			if (fil_bin_w[f][i][j] == 1)
+			{
+				conv_img1 += (t->data)[offset(t, cur_image1, c+j, r+i, 0)];
+				conv_img2 += (t->data)[offset(t, cur_image2, c+j, r+i, 0)];
+			}
+			else
+			{
+				conv_img1 -= (t->data)[offset(t, cur_image1, c+j, r+i, 0)];
+				conv_img2 -= (t->data)[offset(t, cur_image2, c+j, r+i, 0)];
+			}
+		}
+	}
+
+	conv_img1 *= alphas[f];
+	conv_img2 *= alphas[f];
+
+	double bias = fil_b.data[offset(&fil_b, f, 0, 0, 0)];
+
+	conv_img1 += bias;
+	conv_img2 += bias;
+
+	// applying ReLU
+	if (conv_img1 < 0.0)
+	{
+		conv_img1 = 0.0;
+	}
+
+	if (conv_img2 < 0.0)
+	{
+		conv_img2 = 0.0;
+	}
+
+	(conv_t->data)[offset(conv_t,b  ,j,i,f)] = conv_img1;
+	(conv_t->data)[offset(conv_t,b+1,j,i,f)] = conv_img2;
 }
 
 void xnor_convolution(int bin_input_images[BATCH_SIZE][IMAGE_ROWS][IMAGE_COLS], double betas[BATCH_SIZE][N_ROWS_CONV][N_COLS_CONV], 
@@ -130,38 +266,6 @@ double convolve(tensor* t, int r, int c, int image_num, tensor* fil_w, tensor* f
 	}
 
 	conv_val += (fil_b->data)[offset(fil_b, f, 0, 0, 0)];
-
-	// applying ReLU
-	if (conv_val < 0.0)
-	{
-		conv_val = 0.0;
-	}
-
-	return conv_val;
-}
-
-double bin_convolve(tensor* t, int r, int c, int image_num, int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], double alphas[NUM_FILS],
-	tensor fil_b, int f, int shuffle_index[]){
-
-	double conv_val = 0.0;
-	for (int i = 0; i < FIL_ROWS; ++i)
-	{
-		for (int j = 0; j < FIL_COLS; ++j)
-		{
-			if (fil_bin_w[f][i][j] == 1)
-			{
-				conv_val += (t->data)[offset(t, shuffle_index[image_num], c+j, r+i, 0)];
-			}
-			else
-			{
-			conv_val -= (t->data)[offset(t, shuffle_index[image_num], c+j, r+i, 0)];
-			}
-		}
-	}
-
-	conv_val *= alphas[f];
-
-	conv_val += fil_b.data[offset(&fil_b, f, 0, 0, 0)];
 
 	// applying ReLU
 	if (conv_val < 0.0)
--- src/conv_layer.h
+++ src/conv_layer.h
@@ -8,8 +8,10 @@
 #include "common.h"
 
 double convolve(tensor* t, int r, int c, int image_num, tensor* fil_w, tensor* fil_b, int f, int shuffle_index[]);
-double bin_convolve(tensor* t, int r, int c, int image_num, int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], double alphas[NUM_FILS],
-	tensor fil_b, int f, int shuffle_index[]);
+
+void bin_convolve(tensor* t, tensor* conv_t, int b, int r, int c, int cur_image1, int cur_image2, 
+		int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], double alphas[NUM_FILS], tensor fil_b, int f);
+
 double xnor_convolve(int t[NUM_IMAGES][IMAGE_ROWS][IMAGE_COLS], double betas[BATCH_SIZE][N_ROWS_CONV][N_COLS_CONV],
 						int r, int c, int b, int fil_bin_w[NUM_FILS][FIL_ROWS][FIL_COLS], 
 						double alphas[NUM_FILS], tensor fil_b, int f);
